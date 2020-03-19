aeeb5b031f321aa64b6f8c669b24516f4669a97b
Andrei Ursulean <andreiursulean98@gmail.com>
2020-03-19 19:25:40 +0200

    commit all files

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 /*================== MAIN FUNCTION ==================*/
 function main()
-{
+{   
+    document.getElementById('body').style.display = 'block';
+    document.getElementById('inputsWrapper').style.display = 'none';
+
     // Read inputs
 
-    //let deltaT = 8600;
-    //let n = 100;
     let deltaT = parseInt(document.getElementById('t').value);
     let n = parseInt(document.getElementById('n').value);
     
@@ -152,4 +153,28 @@ function checkElementInArray(val, arr) {
     }
 
     return false;
+}
+
+function openTab(evt, tabName) {
+    
+    // Hide all tabcontent elements
+    let tabcontent = document.getElementsByClassName('tabcontent');
+
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
+
+
+    // set tablinks not active 
+    let tablinks = document.getElementsByClassName('tablinks');
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace('active', '');
+    }
+
+    // Show current tab content
+    document.getElementById(tabName).style.display = 'block';
+
+    // Set current tab link active
+    evt.currentTarget.className += ' active';
+
 }
